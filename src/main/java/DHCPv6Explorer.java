4a5d18815d411d0808b286800065e0f78a74236d
Dmytro Orlov <orlov.dm@gmail.com>
2023-01-09 21:52:43 +0100

    2023-01-09-21-52-43

--- src/main/java/DHCPv6Explorer.java
+++ src/main/java/DHCPv6Explorer.java
@@ -4,14 +4,35 @@ import java.util.Enumeration;
 import java.util.Iterator;
 import java.util.List;
 
-public class DHCPv6Explorer extends Thread {
+public class DHCPv6Explorer {
     private DatagramSocket socket;
     private boolean running;
     private byte[] buf = new byte[256];
     private String multicastAdressPrefix = "FF32:00FF:";
 
+    /*
     public DHCPv6Explorer() throws SocketException {
         socket = new DatagramSocket(4445);
+    }
+    */
+
+    public static void main(String[] args) {
+        try {
+            // showNetwork();
+            // fe80:0:0:0:1c8e:5929:a87b:9111%en0 with Prefix-Length 64
+            var socket = new DatagramSocket();
+            var buf = hexStringtoByteArray(
+                    "600f07f300083afffe80000000000000" +
+                            "1c8e5929a87b9111ff02000000000000" +
+                            "00000000000000028500cdf200000000"
+            );
+            InetAddress address = Inet6Address.getByName("ff02::2");
+            var packet = new DatagramPacket(buf, buf.length, address, 0);
+            socket.send(packet);
+            socket.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public void run() {
@@ -46,7 +67,7 @@ public class DHCPv6Explorer extends Thread {
     }
 
 
-    private byte[] hexStringtoByteArray(String hex) {
+    private static byte[] hexStringtoByteArray(String hex) {
         /* Konvertiere den String mit Hex-Ziffern in ein Byte-Array */
         byte[] val = new byte[hex.length() / 2];
         for (int i = 0; i < val.length; i++) {
@@ -57,7 +78,7 @@ public class DHCPv6Explorer extends Thread {
         return val;
     }
 
-    private String byteArraytoHexString(byte[] byteArray) {
+    private static String byteArraytoHexString(byte[] byteArray) {
         /* Konvertiere das Byte-Array in einen String mit Hex-Ziffern */
         String hex = "";
         if (byteArray != null) {
@@ -68,7 +89,7 @@ public class DHCPv6Explorer extends Thread {
         return hex;
     }
 
-    private void showNetwork() throws SocketException {
+    private static void showNetwork() throws SocketException {
         /* Netzwerk-Infos fuer alle Interfaces ausgeben */
         Enumeration<NetworkInterface> en = NetworkInterface.getNetworkInterfaces();
         while (en.hasMoreElements()) {
@@ -90,9 +111,6 @@ public class DHCPv6Explorer extends Thread {
     }
 
     private Inet6Address getAdress(String host, byte[] addr, int scopeId) throws UnknownHostException {
-
-        return Inet6Address.getByAddress(host , addr, scopeId);
+        return Inet6Address.getByAddress(host, addr, scopeId);
     }
-
-
 }
